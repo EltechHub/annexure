86b812b158970d63c74098533df2d68bcf3a0b19
Elmir <elyor.majidovich@gmail.com>
2022-09-14 17:48:32 +0500

    Update JurnalModel.js

--- models/JurnalModel.js
+++ models/JurnalModel.js
@@ -1,14 +1,8 @@
 const {Sequelize} =require("sequelize");
-//const db =require("../config/database");
-const connection=require('../config/database');
+const db =require("../config/database");
 
 
-const sequelize = new Sequelize('shirinmeva', 'root', '', {
-    host: 'localhost',
-    dialect: 'mysql'
-});
-
-const Jurnal = sequelize.define('product', {
+const Jurnal = db.define('product', {
     title: {
         type: Sequelize.STRING,
     },
@@ -35,54 +29,4 @@ const Jurnal = sequelize.define('product', {
     timestamps: false
 });
 
-/*Jurnal.findAll().then(jurnals => {
-    console.log("All jurnals:", JSON.stringify(jurnals, null, 4));
-});*/
-
 module.exports=Jurnal;
-/*const Jurnal = (sequelize, Sequelize) => {
-    const {INTEGER, STRING, FLOAT, BOOLEAN, DATE} = Sequelize
-    const Jurnals = sequelize.define('new_table', {
-        id: {type: STRING, primaryKey: true, autoIncrement: true},
-        text: {type: STRING, require: true},
-        name: {type: STRING, require: true},
-
-    })
-    return Jurnals;
-}
-
-module.exports = Jurnal*/
-
-//module.exports=model('Jurnal', Jurnal);
-
-/*const {Schema, model}= require('mysql');
-
-const JurnalModel=new Schema({
-    text:{
-        type: String,
-        require: true,
-        minLength:3
-    },
-    name:{
-        type:String,
-        require: true,
-    }
-})
-
-const {DataTypes} =Sequelize;
-const Jurnal=db.define('new_table',{
-
-    text:DataTypes.STRING,
-    name:DataTypes.STRING
-},
-    {
-        freezeTableName:true
-    })
-
-module.exports= Jurnal;
-
-(async ()=>{
-await db.sync()
-})();*/
-
-//module.exports=model('Jurnal', JurnalModel);
